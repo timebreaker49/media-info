6ce7923a8db370fb2c08aacf6d8642c45321a135
timebreaker49 <wcsbaylor@gmail.com>
2021-03-22 17:45:24 -0500

    Added .env file

--- django_react_backend/django_react_backend/settings.py
+++ django_react_backend/django_react_backend/settings.py
@@ -9,18 +9,20 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jq$a$nqzk1t82n7han+9%7@oh3t*u8s0m9)#kv&g+#1823bwk5'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
